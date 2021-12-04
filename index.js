fef41d51c8d8d06c31bad9d79529d6be45e595c8
jacobjuneau6 <79861515+jacobjuneau6@users.noreply.github.com>
2021-12-04 05:17:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-/* alert(
-  "This utility currently does not work due to major changes made to Blooket and my lack of time. Please wait until I get more time and motivation to work on this. Thanks for supporting me!"
-);
+
 
  (async () => {
   const defaultPrefs = {
